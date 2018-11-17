0e22aa1dd8b7a0ae66c643494c5b23dd542b1263
Jesi Namora <ecimoramora@gmail.com>
2018-11-17 09:15:03 -0800

    Lolipop+Add Database

---
+++ client/user/umum/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Admin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+    crossorigin="" />
+  <!-- Make sure you put this AFTER Leaflet's CSS -->
+  <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+    crossorigin=""></script>
+
+  <script src="../../../server/js/main.js"></script>
+
+</head>
+
+<body>
+
+  <?php
+include "include.php";
+include "../../../server/connect.php";
+include "../../view/header.php";
+include "../../view/sidebar.php";
+include "../../view/content.php";
+include "../../view/footer.php";
+
+?>
+
+  
+
+
+</body>
+
+</html>
