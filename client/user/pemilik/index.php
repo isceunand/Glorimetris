02968c2843c920aaf0e3b991f0c3ac96a5eef602
Jesi Namora <ecimoramora@gmail.com>
2018-11-17 14:59:12 -0800

    Oreo+Digitasi Online oleh pemilik

---
+++ client/user/pemilik/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Admin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+    crossorigin="" />
+  <!-- Make sure you put this AFTER Leaflet's CSS -->
+  <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+    crossorigin=""></script>
+
+  <script src="../../../server/js/main.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
+
+
+</head>
+
+<body>
+
+  <?php
+include "include.php";
+include "../../../server/connect.php";
+include "../../view/header.php";
+include "../../view/sidebar.php";
+include "test2.php";
+include "../../view/footer.php";
+
+?>
+
+
+
+<script>
+
+
+
+
+</script>
+
+  
+
+
+</body>
+
+</html>
--- server/proses/radius.php
+++ server/proses/radius.php
@@ -1,7 +1,17 @@
 <?php
 
+
+include '../connect.php';
+
 $radius=$_GET['radius'];
-echo $radius;
 
+// echo $radius;
+$sql = "SELECT ST_AsGeoJSON(ST_Buffer(ST_GeomFromText('POINT(100.33521652221681 -0.8500359541370635)'),100/111319.5)) as data1";
+$result = pg_query($sql);
+while($a = pg_fetch_assoc($result)){
+    $b=$a;
+}
 
+$c=JSON_ENCODE($b);
+echo filter_var($c,FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH)
 ?>
